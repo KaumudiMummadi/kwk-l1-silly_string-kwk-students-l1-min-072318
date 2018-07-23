61608427bf23b54b2629961f158e6b2481cc2808
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-23 20:51:38 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -34,7 +34,7 @@ puts "There are #{letter_count} in #{big_word}"
 
 bigger_word =  big_word + "wow" + "mom"
 
-new_letter_count = bigger_word
+new_letter_count = bigger_word.length
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
